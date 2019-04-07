68573157974115b05f10f07939c0b2b8a6be2eab
hans <laderud@hotmail.com>
2019-04-07 12:10:24 +0200

    full colemak

--- Model01-Firmware.ino
+++ Model01-Firmware.ino
@@ -143,7 +143,7 @@ enum { MACRO_VERSION_INFO,
   *
   */
 
-enum { PRIMARY, COLEMAK, NUMPAD, FUNCTION, GAMING, NORWEGIAN }; // layers
+enum { PRIMARY, NUMPAD, FUNCTION, GAMING, NORWEGIAN }; // layers
 
 
 /**
@@ -162,9 +162,8 @@ enum { PRIMARY, COLEMAK, NUMPAD, FUNCTION, GAMING, NORWEGIAN }; // layers
 // #define PRIMARY_KEYMAP_QWERTY
 // #define PRIMARY_KEYMAP_COLEMAK
 // #define PRIMARY_KEYMAP_DVORAK
- #define PRIMARY_KEYMAP_CUSTOM
-// #define PRIMARY_KEYMAP_CUSTOM_TARMAK
-// #define PRIMARY_KEYMAP_CUSTOM_COLEMAK
+// #define PRIMARY_KEYMAP_CUSTOM
+ #define PRIMARY_KEYMAP_CUSTOM_COLEMAK
 
 
 /* This comment temporarily turns off astyle's indent enforcement
@@ -239,26 +238,7 @@ KEYMAPS(
     RALT(LCTRL(Key_LeftShift)), Key_6, Key_7, Key_8,     Key_9,         Key_0,         LockLayer(GAMING),//LockLayer(NUMPAD),
     Key_Enter,                  Key_Y, Key_U, Key_I,     Key_O,         Key_P,         Key_Equals,
                                 Key_H, Key_J, Key_K,     Key_L,         Key_Semicolon, Key_Quote,
-    LockLayer(COLEMAK),         Key_N, Key_M, Key_Comma, Key_Period,    Key_Slash,     Key_Minus,
-    OSM(RightShift), OSM(LeftAlt), Key_Spacebar, OSM(RightControl),
-    OSL(FUNCTION)
-),
-
-#elif defined (PRIMARY_KEYMAP_CUSTOM_TARMAK)
-  // CURRENTLY: TARMAK#1
-  [PRIMARY] = KEYMAP_STACKED
-(
-    LockLayer(NORWEGIAN), Key_1, Key_2, Key_3, Key_4, Key_5, LSHIFT(Key_LeftGui),
-    Key_Backtick,         Key_Q, Key_W, Key_J, Key_R, Key_T, Key_Tab,
-    Key_PageUp,           Key_A, Key_S, Key_D, Key_F, Key_G,
-    Key_PageDown,         Key_Z, Key_X, Key_C, Key_V, Key_B, Key_Escape,
-    OSM(LeftControl), Key_Backspace, OSM(LeftGui), OSM(LeftShift),
-    OSL(FUNCTION),
-    
-    RALT(LCTRL(Key_LeftShift)), Key_6, Key_7, Key_8,     Key_9,         Key_0,         LockLayer(GAMING),//LockLayer(NUMPAD),
-    Key_Enter,                  Key_Y, Key_U, Key_I,     Key_O,         Key_P,         Key_Equals,
-                                Key_H, Key_N, Key_E,     Key_L,         Key_Semicolon, Key_Quote,
-    Key_RightAlt,               Key_K, Key_M, Key_Comma, Key_Period,    Key_Slash,     Key_Minus,
+    Key_RightAlt,               Key_N, Key_M, Key_Comma, Key_Period,    Key_Slash,     Key_Minus,
     OSM(RightShift), OSM(LeftAlt), Key_Spacebar, OSM(RightControl),
     OSL(FUNCTION)
 ),
@@ -286,24 +266,6 @@ KEYMAPS(
 #error "No default keymap defined. You should make sure that you have a line like '#define PRIMARY_KEYMAP_QWERTY' in your sketch"
 
 #endif
-
-  [COLEMAK] = KEYMAP_STACKED
-(
-    LockLayer(NORWEGIAN), Key_1, Key_2, Key_3, Key_4, Key_5, LSHIFT(Key_LeftGui),
-    Key_Backtick,         Key_Q, Key_W, Key_F, Key_P, Key_G, Key_Tab,
-    Key_PageUp,           Key_A, Key_R, Key_S, Key_T, Key_D,
-    Key_PageDown,         Key_Z, Key_X, Key_C, Key_V, Key_B, Key_Escape,
-    OSM(LeftControl), Key_Backspace, OSM(LeftGui), OSM(LeftShift),
-    OSL(FUNCTION),
-    
-    RALT(LCTRL(Key_LeftShift)), Key_6, Key_7, Key_8,     Key_9,       Key_0,         LockLayer(GAMING),//LockLayer(NUMPAD),
-    Key_Enter,                  Key_J, Key_L, Key_U,     Key_Y,       Key_Semicolon, Key_Equals,
-                                Key_H, Key_N, Key_E,     Key_I,       Key_O,		   Key_Quote,
-    UnlockLayer(COLEMAK),       Key_K, Key_M, Key_Comma, Key_Period,  Key_Slash,     Key_Minus,
-    OSM(RightShift), OSM(LeftAlt), Key_Spacebar, OSM(RightControl),
-    OSL(FUNCTION)
-),
-
 
   [NUMPAD] =  KEYMAP_STACKED
   (___, ___, ___, ___, ___, ___, ___,
@@ -361,8 +323,8 @@ KEYMAPS(
    ___,
 
    ___, ___, ___, ___, ___, ___, ___,
-   ___, ___, ___, ___, ___, ___, Key_F24,
-   ___, ___, ___, ___, Key_F23, Key_F22,
+   ___, ___, ___, ___, ___, Key_F23, Key_F24,
+   ___, ___, ___, ___, ___, Key_F22,
    ___, ___, ___, ___, ___, ___, ___,
    ___, ___, ___, ___,
    ___)
